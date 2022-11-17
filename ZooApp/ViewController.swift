a20a0cffac4cbcb35862bc2e81e939ec6b04c9e6
IanO12 <ostrandi@mail.gvsu.edu>
2022-11-16 20:24:33 -0500

    spawning objects

--- ZooApp/ViewController.swift
+++ ZooApp/ViewController.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 import RealityKit
-
+import ARKit
 class ViewController: UIViewController {
     
     @IBOutlet var arView: ARView!
@@ -15,10 +15,58 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        // Load the "Box" scene from the "Experience" Reality File
-        let boxAnchor = try! Experience.loadBox()
+//         Load the "Box" scene from the "Experience" Reality File
+//        let boxAnchor = try! Experience.loadBox()
         
-        // Add the box anchor to the scene
-        arView.scene.anchors.append(boxAnchor)
+//         Add the box anchor to the scene
+//        arView.scene.anchors.append(boxAnchor)
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        arView.session.delegate = self
+
+        setupARView()
+
+        arView.addGestureRecognizer(UITapGestureRecognizer(target:self,action: #selector(handleTap(recognizer:))))
+    }
+    
+    func setupARView(){
+        arView.automaticallyConfigureSession = false
+        let configuration = ARWorldTrackingConfiguration()
+        configuration.planeDetection = [.horizontal, .vertical]
+        configuration.environmentTexturing = .automatic
+        arView.session.run(configuration)
+    }
+
+    @objc
+    func handleTap(recognizer: UITapGestureRecognizer){
+        let location = recognizer.location(in: arView)
+        let results = arView.raycast(from: location, allowing: .estimatedPlane, alignment: .horizontal)
+        if let firstResult = results.first{
+            let anchor = ARAnchor(name: "Cube", transform: firstResult.worldTransform)
+            arView.session.add(anchor: anchor)
+        } else{
+            print("Object Placement Failed")
+        }
+    }
+
+    func placeObject(named entityName: String, for anchor: ARAnchor){
+        let cube = MeshResource.generateBox(size: 0.3)
+        let entity = ModelEntity(mesh: cube)
+        let anchorEntity = AnchorEntity(anchor: anchor)
+        anchorEntity.addChild(entity)
+        arView.scene.addAnchor(anchorEntity)
+    }
+}
+extension ViewController: ARSessionDelegate {
+    func session(_ session: ARSession, didAdd anchors: [ARAnchor]) {
+        for anchor in anchors{
+            if let anchorName = anchor.name, anchorName == "Cube"{
+                print(anchor)
+                placeObject(named: anchorName, for: anchor)
+            }
+        }
     }
 }
