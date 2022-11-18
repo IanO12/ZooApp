f451faabb3cbdbe6fe4ee9c114d9f883f5da01bb
Jaxon <gogginja@mail.gvsu.edu>
2022-11-18 14:01:19 -0500

    save and load functions

--- ZooApp/ViewController.swift
+++ ZooApp/ViewController.swift
@@ -12,52 +12,115 @@ class ViewController: UIViewController {
     
     @IBOutlet var arView: ARView!
     
+    @IBOutlet weak var LoadWorld: UIButton!
+    @IBOutlet weak var newView: ARView!
+    @IBOutlet weak var SaveWorld: UIButton!
+    var worldMapURL: URL = {
+        do {
+            return try FileManager.default.url(for: .documentDirectory, in: .userDomainMask, appropriateFor: nil, create: true)
+                .appendingPathComponent("worldMapURL")
+        } catch {
+            fatalError("Error getting world map URL from document directory.")
+        }
+    }()
+    var defaultConfiguration: ARWorldTrackingConfiguration {
+            let configuration = ARWorldTrackingConfiguration()
+            configuration.planeDetection = .horizontal
+            configuration.environmentTexturing = .automatic
+            return configuration
+        }
     override func viewDidLoad() {
         super.viewDidLoad()
         
-//         Load the "Box" scene from the "Experience" Reality File
-//        let boxAnchor = try! Experience.loadBox()
+        //         Load the "Box" scene from the "Experience" Reality File
+        //        let boxAnchor = try! Experience.loadBox()
         
-//         Add the box anchor to the scene
-//        arView.scene.anchors.append(boxAnchor)
+        //         Add the box anchor to the scene
+        //       arView.scene.anchors.append(boxAnchor)
     }
     
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
         
-        arView.session.delegate = self
-
+        newView.session.delegate = self
+        
         setupARView()
-
-        arView.addGestureRecognizer(UITapGestureRecognizer(target:self,action: #selector(handleTap(recognizer:))))
+        
+        newView.addGestureRecognizer(UITapGestureRecognizer(target:self,action: #selector(handleTap(recognizer:))))
     }
     
     func setupARView(){
-        arView.automaticallyConfigureSession = false
+        newView.automaticallyConfigureSession = false
         let configuration = ARWorldTrackingConfiguration()
         configuration.planeDetection = [.horizontal, .vertical]
         configuration.environmentTexturing = .automatic
-        arView.session.run(configuration)
+        newView.session.run(configuration)
     }
-
+    
     @objc
     func handleTap(recognizer: UITapGestureRecognizer){
-        let location = recognizer.location(in: arView)
-        let results = arView.raycast(from: location, allowing: .estimatedPlane, alignment: .horizontal)
+        let location = recognizer.location(in: newView)
+        let results = newView.raycast(from: location, allowing: .estimatedPlane, alignment: .horizontal)
         if let firstResult = results.first{
             let anchor = ARAnchor(name: "Cube", transform: firstResult.worldTransform)
-            arView.session.add(anchor: anchor)
+            newView.session.add(anchor: anchor)
         } else{
             print("Object Placement Failed")
         }
     }
-
+    
     func placeObject(named entityName: String, for anchor: ARAnchor){
         let cube = MeshResource.generateBox(size: 0.3)
         let entity = ModelEntity(mesh: cube)
         let anchorEntity = AnchorEntity(anchor: anchor)
         anchorEntity.addChild(entity)
-        arView.scene.addAnchor(anchorEntity)
+        newView.scene.addAnchor(anchorEntity)
+    }
+    func writeWorldMap(_ worldMap: ARWorldMap, to url: URL) throws {
+        let data = try NSKeyedArchiver.archivedData(withRootObject: worldMap, requiringSecureCoding: true)
+        try data.write(to: url)
+    }
+    @IBAction func saveButtonAction(_ sender: Any) {
+        print("Working")
+        newView.session.getCurrentWorldMap { (worldMap, error) in
+            guard let worldMap = worldMap else {
+                //self.showAlert(title: "No Map", message: error!.localizedDescription); return
+                print("No map")
+                return
+            }
+            do {
+                let data = try NSKeyedArchiver.archivedData(withRootObject: worldMap, requiringSecureCoding: true)
+                try data.write(to: self.worldMapURL, options: [.atomic])
+                //self.showAlert(message: "Map Saved")
+            } catch {
+                fatalError("Can't save map: \(error.localizedDescription)")
+            }
+        }
+    }
+    func loadWorldMap(from url: URL) throws -> ARWorldMap {
+        let mapData = try Data(contentsOf: url)
+        guard let worldMap = try NSKeyedUnarchiver.unarchivedObject(ofClass: ARWorldMap.self, from: mapData)
+        else { throw ARError(.invalidWorldMap) }
+        return worldMap
+    }
+    var mapDataFromFile: Data? {
+        return try? Data(contentsOf: worldMapURL)
+    }
+    @IBAction func loadButtonAction(_ sender: Any) {
+        let worldMap: ARWorldMap = {
+                    guard let data = mapDataFromFile
+                        else { fatalError("Map data should already be verified to exist before Load button is enabled.") }
+                    do {
+                        guard let worldMap = try NSKeyedUnarchiver.unarchivedObject(ofClass: ARWorldMap.self, from: data)
+                            else { fatalError("No ARWorldMap in archive.") }
+                        return worldMap
+                    } catch {
+                        fatalError("Can't unarchive ARWorldMap from file data: \(error)")
+                    }
+                }()
+        let configuration = self.defaultConfiguration
+        configuration.initialWorldMap = worldMap
+        newView.session.run(configuration, options: [.resetTracking, .removeExistingAnchors])
     }
 }
 extension ViewController: ARSessionDelegate {
